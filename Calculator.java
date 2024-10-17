3e7f7db05796e675c94f082e20a5eb9962bd93cd
Geetanshi23 <geetanshipahuja@gmail.com>
2024-10-17 15:16:49 +0530

    Switch: it is done

--- Calculator.java
+++ Calculator.java
@@ -1,16 +1,32 @@
 import java.util.*;
-public class Calculator{
-    public static void main(String[] args){
-		int a = 10;
-		int b = 10;
-		int c = a+b;
-		System.out.println(c);
-		c = a-b;
-		System.out.println(c);
-		c = a*b;
-		System.out.println(c);
-		c = a/b;
-		System.out.println(c);
-		System.out.println("Welcome");
-    }
+public class Calculator {
+	public static void main(String[] args)
+	{
+		double num1, num2;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the numbers:");
+		num1 = sc.nextDouble();
+		num2 = sc.nextDouble();
+		System.out.println("Enter operator (+,-,*,/):");
+		char op = sc.next().charAt(0);
+		double o = 0;
+		switch (op) {
+		case '+':
+			o = num1 + num2;
+			break;
+		case '-':
+			o = num1 - num2;
+			break;
+		case '*':
+			o = num1 * num2;
+			break;
+		case '/':
+			o = num1 / num2;
+			break;
+		}
+		System.out.println("result:");
+		System.out.println();
+		System.out.println(num1 + " " + op + " " + num2
+						+ " = " + o);
+	}
 }
